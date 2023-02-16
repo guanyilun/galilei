09754d3c6f740dbddcc2e623a272e0933d5f772c
Yilun Guan <zoom.aaron@gmail.com>
2023-02-15 23:53:58 -0500

    add docstring

--- galilei/emulator.py
+++ galilei/emulator.py
@@ -14,6 +14,18 @@ from tqdm import tqdm
 
 class Net(nn.Module):
     def __init__(self, idim, odim, hidden=[64, 64]):
+        """
+        Simple fully connected neural network with ReLU activation
+
+        Parameters
+        ----------
+        idim : int
+            Input dimension
+        odim : int
+            Output dimension
+        hidden : list of int
+            List of hidden layer sizes
+        """
         super(Net, self).__init__()
         self.layer0 = nn.Linear(idim, hidden[0])
         for i in range(1, len(hidden)):
@@ -34,11 +46,43 @@ class Emulator:
         test_size=0.2,
         preconditioner=None,
     ):
+        """
+        Base class for emulators
+
+        Parameters
+        ----------
+        test_size : float
+            Fraction of samples to use for testing
+        preconditioner : object
+            Implement forward and backward methods for transforming function results before
+            training and after prediction
+        """
         self.test_size = test_size
         self.preconditioner = preconditioner  # need to have forward and backward methods for transf and inverse transf
         self.model = None
 
     def _prepare_data(self, func, samples):
+        """
+        Prepare data for training and testing
+
+        Parameters
+        ----------
+        func : callable
+            Function to be emulated
+        samples : dict
+            Dictionary of parameter names and values
+
+        Returns
+        -------
+        X_train : np.ndarray
+            Training input
+        X_test : np.ndarray
+            Test input
+        Y_train : np.ndarray
+            Training output
+        Y_test : np.ndarray
+            Test output
+        """
         X = np.vstack([np.array(v) for _, v in samples.items()]).T
 
         # run all combinations and gather results
@@ -65,15 +109,63 @@ class Emulator:
         return X_train, X_test, Y_train, Y_test
 
     def _train(self, X_train, Y_train):
+        """
+        Train the emulator
+
+        Parameters
+        ----------
+        X_train : np.ndarray
+            Training input
+        Y_train : np.ndarray
+            Training output
+        """
         raise NotImplementedError
 
     def _test(self, X_test, Y_test):
+        """
+        Test the emulator
+
+        Parameters
+        ----------
+        X_test : np.ndarray
+            Test input
+        Y_test : np.ndarray
+            Test output
+        """
         raise NotImplementedError
 
     def _predict(self, x):
+        """
+        Predict the output for a given input
+
+        Parameters
+        ----------
+        x : np.ndarray
+            Input parameters for emulated function
+
+        Returns
+        -------
+        y : np.ndarray
+            Output of emulated function
+        """
         raise NotImplementedError
 
     def _build_func(self, param_keys):
+        """
+        Build a function that can be used to emulate the original function
+
+        Parameters
+        ----------
+        param_keys : list of str
+            List of parameter names
+
+        Returns
+        -------
+        emulated_func : callable
+            Function that can be used to emulate the original function. The function has a
+            reference to the emulator instance in the attribute `emulator`.
+        """
+
         def emulated_func(**kwargs):
             values = [kwargs.get(k, None) for k in param_keys]
             if None in values:
@@ -83,12 +175,28 @@ class Emulator:
                 res = self.preconditioner.backward(res)
             return res
 
-        # attach a reference to the model and emulator instance in the function
-        emulated_func.model = self.model
+        # attach a reference to the emulator instance in the function
         emulated_func.emulator = self
         return emulated_func
 
     def emulate(self, func, samples):
+        """
+        Emulate a function
+
+        Parameters
+        ----------
+        func : callable
+            Function to be emulated
+        samples : dict
+            Dictionary of parameter names and values, where the values are lists of
+            parameter values to evaluate the function at to build the emulator
+
+        Returns
+        -------
+        emulated_func : callable
+            Function that can be used to emulate the original function. The function has a
+            reference to the emulator instance in the attribute `emulator`.
+        """
         X_train, X_test, Y_train, Y_test = self._prepare_data(func, samples)
         self._train(X_train, Y_train)
         self._test(X_test, Y_test)
@@ -109,6 +217,41 @@ class TorchEmulator(Emulator):
         NN_kwargs={},
         **kwargs,
     ):
+        """
+        A class used to emulate data using a neural network based on the PyTorch library.
+
+        Parameters
+        ----------
+        NNClass : nn.Module, optional
+            The neural network class to use. Default is Net.
+        epochs : int, optional
+            The number of epochs to train the neural network. Default is 500.
+        optimizer : torch.optim.Optimizer, optional
+            The optimizer to use for training the neural network. If None, uses Stochastic Gradient Descent (SGD) optimizer with default parameters.
+        criterion : torch.nn.modules.loss._Loss, optional
+            The loss function to use for training the neural network. If None, uses Mean Squared Error (MSE) loss function.
+        lr : float, optional
+            The learning rate for the optimizer. Default is 0.01.
+        weight_decay : float, optional
+            The weight decay (L2 penalty) for the optimizer. Default is 0.
+        momentum : float, optional
+            The momentum factor for the optimizer. Default is 0.9.
+        NN_kwargs : dict, optional
+            The keyword arguments to pass to the neural network class constructor. Default is an empty dictionary.
+        **kwargs
+            Additional keyword arguments to pass to the base Emulator class constructor.
+
+        Attributes
+        ----------
+        NNClass : callable
+            A callable that returns an instance of the neural network class with the given keyword arguments.
+        epochs : int
+            The number of epochs to train the neural network.
+        optimizer : torch.optim.Optimizer
+            The optimizer to use for training the neural network.
+        criterion : torch.nn.modules.loss._Loss
+            The loss function to use for training the neural network.
+        """
         super(TorchEmulator, self).__init__(**kwargs)
         self.NNClass = partial(NNClass, **NN_kwargs)
         self.epochs = epochs
@@ -120,6 +263,16 @@ class TorchEmulator(Emulator):
         self.criterion = criterion if criterion is not None else nn.MSELoss()
 
     def _train(self, X_train, Y_train):
+        """
+        Train the emulator
+
+        Parameters
+        ----------
+        X_train : np.ndarray
+            Training input
+        Y_train : np.ndarray
+            Training output
+        """
         # now we can train a model
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -157,6 +310,16 @@ class TorchEmulator(Emulator):
         self.model = model
 
     def _test(self, X_test, Y_test):
+        """
+        Test the emulator on a test set
+
+        Parameters
+        ----------
+        X_test : array-like
+            The test set input data
+        Y_test : array-like
+            The test set output data
+        """
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         X_test = torch.as_tensor(X_test, dtype=torch.float32, device=device)
         Y_test = torch.as_tensor(Y_test, dtype=torch.float32, device=device)
@@ -173,6 +336,19 @@ class TorchEmulator(Emulator):
             print("Ave Test loss: {:.3f}".format(avg_test_loss))
 
     def _predict(self, x):
+        """
+        Predict the output of the emulator for a given input
+
+        Parameters
+        ----------
+        x : array-like
+            The input data
+
+        Returns
+        -------
+        np.ndarray
+            The predicted output
+        """
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         x = torch.as_tensor(x, dtype=torch.float32, device=device)
         res = self.model(x).detach().cpu().numpy()
@@ -187,6 +363,26 @@ class SklearnEmulator(Emulator):
         model_kwargs={"n_restarts_optimizer": 5},
         **kwargs,
     ):
+        """
+        A class used to emulate data using a machine learning model based on the scikit-learn library.
+
+        Parameters
+        ----------
+        model : sklearn.base.TransformerMixin, optional
+            The machine learning model to use. If None, uses a Gaussian Process model with an RBF kernel from scikit-learn with default parameters.
+        kernel_kwargs : dict, optional
+            The keyword arguments to pass to the RBF kernel constructor if a Gaussian Process model is used. Default is an empty dictionary.
+        model_kwargs : dict, optional
+            The keyword arguments to pass to the machine learning model constructor if one is provided. Default sets n_restarts_optimizer to 5.
+        **kwargs
+            Additional keyword arguments to pass to the base Emulator class constructor.
+
+        Attributes
+        ----------
+        model : sklearn.base.TransformerMixin
+            The machine learning model used for emulation.
+        """
+
         super(SklearnEmulator, self).__init__(**kwargs)
         if model is None:
             # use a gaussian process model with RKB kernel from sklearn if none is provided
@@ -196,14 +392,47 @@ class SklearnEmulator(Emulator):
         self.model = model
 
     def _train(self, X_train, Y_train):
+        """
+        Train the emulator
+
+        Parameters
+        ----------
+        X_train : np.ndarray
+            Training input
+        Y_train : np.ndarray
+            Training output
+        """
         self.model.fit(X_train, Y_train)
 
     def _test(self, X_test, Y_test):
+        """
+        Test the emulator on a test set
+
+        Parameters
+        ----------
+        X_test : array-like
+            The test set input data
+        Y_test : array-like
+            The test set output data
+        """
         if len(X_test) > 0:
             test_loss = mean_squared_error(Y_test, self.model.predict(X_test))
             print("Ave Test loss: {:.3f}".format(test_loss))
 
     def _predict(self, x):
+        """
+        Predict the output of the emulator for a given input
+
+        Parameters
+        ----------
+        x : array-like
+            The input data
+
+        Returns
+        -------
+        np.ndarray
+            The predicted output
+        """
         res = self.model.predict([x])[0]
         return res
 
@@ -217,6 +446,35 @@ class emulate:
         backend="torch",
         **kwargs,
     ):
+        """
+        A class used to emulate a function using a machine learning model.
+
+        Parameters
+        ----------
+        samples : np.ndarray or pd.DataFrame, optional
+            The samples to use for training the machine learning model. If None, the emulator will not be trained and cannot be used for emulation.
+        emulator_class : type, optional
+            The class of the emulator to use. If None, the default emulator for the specified backend will be used.
+        backend : str, optional
+            The backend library to use for the emulator. Currently supported options are "torch" and "sklearn". Default is "torch".
+        **kwargs
+            Additional keyword arguments to pass to the emulator constructor.
+
+        Attributes
+        ----------
+        samples : np.ndarray or pd.DataFrame
+            The samples used for training the machine learning model.
+        emulator_class : type
+            The class of the emulator used for emulation.
+        em : emulator_class
+            An instance of the emulator used for emulation.
+
+        Methods
+        -------
+        __call__(func)
+            Call the emulator to emulate the specified function with the specified input samples.
+        """
+
         self.samples = samples
 
         # select the backend
@@ -234,4 +492,17 @@ class emulate:
         self.em = self.emulator_class(**kwargs)
 
     def __call__(self, func):
+        """
+        Call the emulator to emulate the specified function with the specified input samples.
+
+        Parameters
+        ----------
+        func : callable
+            The function to emulate.
+
+        Returns
+        -------
+        callable
+            The emulator function.
+        """
         return self.em.emulate(func, self.samples)
