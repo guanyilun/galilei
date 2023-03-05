15321f42ac492c66f9741a51dc923bbb33e88384
Yilun Guan <zoom.aaron@gmail.com>
2023-03-05 09:36:28 -0500

    experimental: add missing docstrings and add samples in output

--- galilei/experimental.py
+++ galilei/experimental.py
@@ -10,28 +10,46 @@ class collect:
 
         Parameters
         ----------
+        samples: dict
+            dictionary of samples
+        save: str
+            path to save the data
+        mpi: bool
+            use mpi to collect data, default False, only works when
+            mpi4py is installed
         """
         self.samples = samples
         self.mpi = mpi
         self.save = save
 
         if self.mpi:
-            from mpi4py import MPI
+            try:
+                from mpi4py import MPI
 
-            self.comm = MPI.COMM_WORLD
-            self.rank = self.comm.Get_rank()
-            self.size = self.comm.Get_size()
+                self.comm = MPI.COMM_WORLD
+                self.rank = self.comm.Get_rank()
+                self.size = self.comm.Get_size()
+            except ImportError:
+                raise ImportError(
+                    "mpi4py is not installed, " "please install it to use mpi"
+                )
         else:
             self.comm = None
             self.rank = 0
             self.size = 1
 
     def __call__(self, func):
+        """
+        collect data from a function
+
+        Parameters
+        ----------
+        func: function
+            function to collect data from
+        """
         X = np.vstack([np.array(v) for _, v in self.samples.items()]).T
         res_list = []
-        for i in range(
-            self.rank, X.shape[0], self.size
-        ):  # samples has shape (n_samples, n_parameters)
+        for i in range(self.rank, X.shape[0], self.size):
             res = func(**{k: v for k, v in zip(self.samples.keys(), X[i, :])})
             res_list.append(res)
         Y = np.array(res_list)
@@ -49,6 +67,7 @@ class collect:
 
             # save output
             to_save = {
+                "samples": self.samples,
                 "X": X,
                 "Y": Y,
             }
